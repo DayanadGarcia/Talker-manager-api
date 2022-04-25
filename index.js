8aeefab69e87d28c8af4c2b5be8beba71b53c3db
Dayana <dayanadiasgarcia@gmail.com>
2022-04-25 14:57:44 -0300

    Initial commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
-});
+});// 
 
 app.listen(PORT, () => {
   console.log('Online');
