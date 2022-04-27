b2c38fa77da6b7c0c64c2590ba49de5fd9fc8856
Dayana <dayanadiasgarcia@gmail.com>
2022-04-27 15:16:14 -0300

    Requisito 03

--- src/talker.js
+++ src/talker.js
@@ -1,6 +1,9 @@
+const crypto = require('crypto');
 const { readFile } = require('./readFile');
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOTFOUND_STATUS = 404;
+const Unauthorized = 401;
 
 const getTalker = async (req, res) => { // ---------------------função 1 -------------
   const talks = await readFile();
@@ -17,15 +20,23 @@ const findTalker = async (req, res) => { // -------------------------função 2 
   const talkers = talks.find((talk) => talk.id === parseInt(id, 10));
 
   if (!talkers) {
-    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    return res.status(HTTP_NOTFOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
   }
   return res.status(HTTP_OK_STATUS).json(talkers);
 };
 
-const login = (req, res) => res.json({ // -----------------------função 3 ------------------
-    email: 'email@email.com',
-    password: '123456',
-  });
+const login = (req, res) => { // -----------------------função 3 -------------------------
+  try {
+    const { email, password } = req.body;
+    if ([email, password].includes(undefined)) {
+      return res.status(Unauthorized).json({ message: 'missing fields' });
+  }
+    const token = crypto.randomBytes(8).toString('hex');
+    return res.status(HTTP_OK_STATUS).json({ token });
+  } catch (error) {
+    return res.status(500).end();
+  }
+  };
 
 module.exports = {
   getTalker,
